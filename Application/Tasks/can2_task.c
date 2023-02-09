dfd4485baaf2992f7adf56a37ad61282a7ae8ec6
gitromrein <123413005+gitromrein@users.noreply.github.com>
2023-02-09 09:33:15 +0100

    Update can2_task.c

--- Application/Tasks/can2_task.c
+++ Application/Tasks/can2_task.c
@@ -29,6 +29,11 @@
  *********************************************************************/
 #include "cmsis_os.h"
 #include "tasks_defs.h"
+
+//WR
+/* #include "typedefs.h" */
+#include "../Common/typedefs.h"
+
 #include "typedefs.h"
 #include "stm32h7xx.h"
 #include "stm32h7xx_hal_fdcan.h"
